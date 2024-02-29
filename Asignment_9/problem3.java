201ed6f478e7ac546cd068c6982c2ab413373a3a
JOYSTON <116254639+Joystonm@users.noreply.github.com>
2024-02-29 21:53:54 +0530

    update

---
+++ Asignment_9/problem3.java
@@ -0,0 +1,22 @@
+package Asignment_9;
+
+public class problem3 {
+    static char nonrepeat(String s){
+        String res="";
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            int index=s.indexOf(ch,i+1);
+            if(index==-1){
+                res+=ch;
+            }
+        }
+        char f=res.charAt(0);
+        return f;
+     
+    }
+    public static void main(String[] args) {
+        // String s = "hello";
+        String s="zxvczbtxyzvy";
+        System.out.println( nonrepeat(s));
+    }
+}
